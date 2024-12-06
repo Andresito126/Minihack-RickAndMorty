993b0b9d5c1f37e891ebeb8c94b2579903907b8a
diedgo05 <233358@ids.upchiapas.edu.mx>
2024-12-06 13:25:22 -0600

    Servicios e interfaces hechos

---
+++ src/app/Interfaces/characters.ts
@@ -0,0 +1,31 @@
+export interface Characters {
+    id:       number;
+    name:     string;
+    status:   Status;
+    species:  Species;
+    type:     string;
+    gender:   Gender;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  Date;
+}
+
+export enum Gender {
+    Female = "Female",
+    Male = "Male",
+    Unknown = "unknown",
+}
+
+export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+}
+
+export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+}
---
+++ src/app/services/ricknmorty.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable,forkJoin } from 'rxjs';
+import { Characters } from '../Interfaces/characters';
+import { Episode } from '../Interfaces/episode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RicknmortyService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private baseURL = "https://rickandmortyapi.com/api";
+
+
+  // RUTAS PARA LUGARES
+  getLocations():Observable<{Results: Location[]}> {
+    return this.http.get<{Results: Location[]}>(this.baseURL);
+  }
+
+  getLocationByCharacter(id: number): Observable<Location> {
+    return this.http.get<Location>(`${this.baseURL}/character/${id}`);
+  }
+
+  // RUTAS PARA EPISODIOS
+  getEpisodes(): Observable< {results: Episode[]} >{
+    return this.http.get< {results: Episode[]} >(this.baseURL);
+  }
+
+  getEpisodeDetails(id: number): Observable<Episode & { characters: string[] }> {
+    return this.http.get<Episode & { characters: string[] }>(`${this.baseURL}/episode/${id}`);
+  }
+
+  getCharacterDetails(urls: string[]): Observable<Characters[]> {
+    const requests = urls.map(url => this.http.get<Characters>(url));
+    return forkJoin(requests);
+  }
+}
