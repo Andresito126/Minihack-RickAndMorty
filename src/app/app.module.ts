5a55892a8095bc73b59da46e2dac596c2d91e4e7
Andresito126 <creed126ax@gmail.com>
2024-12-06 13:58:32 -0600

    Cards and modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { CharactersModule } from './characters/characters.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    CharactersModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
